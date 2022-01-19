671709f83eb189df06c0fdbe766d1b1e5963ebcd
Abhishek Agarwal <abhishekagarwal151@gmail.com>
2022-01-19 20:40:34 +0530

    Block-4

--- block-BJaaeb/code/script.js
+++ block-BJaaeb/code/script.js
@@ -7,6 +7,11 @@ default value to be "text" and return the input element inside label. (create it
 
 // Your code goes here
 
+function createInputElm(label,type){
+   type='text';
+   return label=document.createElement
+}
+
 // TEST
 createInputElm('Your name'); //<label>Your name: <input type="text"></label>
 createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></label>
