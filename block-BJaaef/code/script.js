0254184ca08a7047fe7d2fa3c6067907bc00ee15
Abhishek Agarwal <abhishekagarwal151@gmail.com>
2022-01-21 14:36:54 +0530

    Block-4||code refactored

--- block-BJaaef/code/script.js
+++ block-BJaaef/code/script.js
@@ -8,17 +8,41 @@
             </li>
 */
 
-got.houses.forEach((house)=>{
-    let li=document.createElement('li');
-    li.classList.add('card');
-    let img=document.createElement('img');
-    img.src=house.people.forEach((person)=> person.image);
-    img.alt=house.people.forEach((person)=>person.wikiLink)
-    let h2=document.createElement('h2');
-    h2.innerText=house.people.forEach((person)=>person.name);
-    let p=document.createElement('p');
-    p.innerText=house.people.forEach((person)=>person.description);
-    let button=document.createElement('button');
-    button.classList.add('btn');
-    li.append(img,h2,p,button);
-})
+
+let allPeople=got.houses.reduce((acc,cv)=>{
+      acc=acc.concat(cv.people);
+      return acc;
+},[]);
+
+let rootElement=document.querySelector('ul');
+
+allPeople.forEach((person)=>{
+  let li=document.createElement('li');
+  li.classList.add('flex-30','card');
+
+  
+  let img=document.createElement('img');
+  img.src=person.image;
+  img.alt=person.name;
+
+  let h2=document.createElement('h2');
+  h2.innerText=person.name;
+  let p=document.createElement('p');
+  p.innerText=person.description;
+  let button=document.createElement('a');
+  button.href=person.wikiLink;
+  button.innerText='Learn More!';
+
+  let head=document.createElement('div');
+  head.classList.add('center');
+  head.append(img,h2);
+
+  let footer=document.createElement('div');
+  footer.classList.add('text-center');
+  footer.append(button);
+
+  li.append(head,p,footer);
+  rootElement.append(li);
+
+
+});
